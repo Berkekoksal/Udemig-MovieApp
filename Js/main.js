60afeb7538abc13dca4351ad9eaf5e8f3b683d6f
Berkekoksal <berkekoksal02@gmail.com>
2024-12-27 21:53:44 +0300

    Project completed.

--- Js/main.js
+++ Js/main.js
@@ -1,11 +1,11 @@
-import { API_TOKEN } from "./config";
+import API_TOKEN from "./config.js";
 
 //* API
 const options = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization: `Bearer ${API_TOKEN}`,
+    Authorization: `${API_TOKEN}`,
   },
 };
 const apiUrl = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`;
